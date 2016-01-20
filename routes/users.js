7bddfb34639cf84495fd85a7d2b885dc6eeff748
葛方帅 <gefangshuai@163.com>
2016-01-20 21:20:04 +0800

    coding

--- routes/users.js
+++ routes/users.js
@@ -4,28 +4,29 @@ var async = require('async');
 var dbHelper = require('../db/dbHelper');
 
 /* GET users listing. */
-router.get('/:username', function(req, res, next) {
+router.get('/:username', function (req, res, next) {
     var username = req.params.username;
     var User = dbHelper.User;
     var Article = dbHelper.Article;
     async.waterfall([
-        function(callback) {
+        function (callback) {
             User.findOne({username: username}).exec(function (err, user) {
                 callback(null, user);
             });
         },
-        function(user, callback) {
-            if(user) {
-                Article.find({_user: user.id}).populate('_user').exec(function(err, articles) {
-                    callback(null, articles);
+        function (user, callback) {
+            if (user) {
+                Article.find({_user: user.id}).populate('_user').exec(function (err, articles) {
+                    callback(null, articles, user);
                 });
-            }else{
+            } else {
                 callback(null, null);
             }
         }
-    ], function (err, articles) {
-        res.render('index', {
+    ], function (err, articles, user) {
+        res.render('my', {
             articles: articles,
+            user: user,
             menu: 'my'
         });
     });
