1bfd8141ada628d0c63b1df03837376b51856ba7
葛方帅 <gefangshuai@163.com>
2016-01-29 11:38:01 +0800

    update

--- routes/api/wx/index.js
+++ routes/api/wx/index.js
@@ -10,10 +10,27 @@ router.get('/', function (req, res, next) {
     var nonce = req.query.nonce;
     var echostr = req.query.echostr;
 
-    if(wxHelper.checkSignature(signature, timestamp, nonce)){
+    if (wxHelper.checkSignature(signature, timestamp, nonce)) {
         res.send(echostr);
-    }else{
+    } else {
         res.send('false');
+    }
+
+});
+
+router.post('/', function (req, res) {
+    var wxApi = req.body;
+    if (wxHelper.checkSignature(wxApi.signature, wxApi.timestamp, wxApi.nonce)) {
+        var reply = "<xml>" +
+            "<ToUserName>" + wxApi.ToUserName + "</ToUserName>" +
+            "<FromUserName>" + wxApi.FromUserName + "</FromUserName>" +
+            "<CreateTime>12345678</CreateTime>" +
+            "<MsgType>text</MsgType>" +
+            "<Content>meg: " + wxApi.Content + "</Content>" +
+            "</xml>";
+        res.send(reply);
+    } else {
+        res.send('auth failed!');
     }
 
 });
